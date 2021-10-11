faed0754fcd544a22a27cf0a8d4cc9a9207f6fd8
Nkrumah <nkrumahthis@gmail.com>
2021-10-11 07:22:44 +0000

    add App

---
+++ src/main/java/com/example/mailbox/App.java
@@ -0,0 +1,23 @@
+package com.example.mailbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    String username;
+
+    public static void main( String[] args )
+    {
+        
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
